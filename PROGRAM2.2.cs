0e8554154589af49d12ae8d601b3dec241587ac9
Aditya Upadhyay <goodboy281103@gmail.com>
2026-08-05 10:12:25 +0530

    Add files via upload

---
+++ PROGRAM2.2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mcaproject_1
+{
+    internal class PROGRAM2
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Num 1");
+            int NUM1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Num 2");
+            int NUM2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sum of Two Numbers is:" + (NUM1 + NUM2));
+
+            Console.WriteLine("Difference is" + (NUM1 - NUM2));
+            Console.WriteLine("Product is:" + (NUM1 * NUM2));
+            Console.WriteLine("Quotient is:" + (NUM1 / NUM2));
+            Console.WriteLine("Remainder is:" + (NUM1 % NUM2));
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mcaproject_1
+{
+    internal class Program 
+    {
+        static void Main1(string[] args)
+        {
+            int rollno;
+            string name;
+            float marks;
+
+            Console.Write("Enter Student Name");
+             name = Console.ReadLine();
+
+            Console.Write("Enter Student Roll  Number");
+             rollno = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter Student Marks");
+             marks = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("\n---Student Details---");
+            Console.WriteLine("student Name:" + name);
+            Console.WriteLine("student Roll number:" + rollno);
+            Console.WriteLine("student Marks:" + marks);
+
+
+        }
+
+     
+    }
+}
