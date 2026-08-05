748349669f3d609dc5494d82bb8f9ca090185016
Aditya Upadhyay <goodboy281103@gmail.com>
2026-08-05 11:34:35 +0530

    Add files via upload

---
+++ PROGRAM3.3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mcaproject_1
+{
+    internal class PROGRAM3
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num < 0)
+            {
+                Console.WriteLine("num is:" + "Negative");
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine("num is:" + "Positive");
+            }
+            else
+            {
+                Console.WriteLine("num is:" + "Zero");
+
+            }
+
+            {
+
+            }
+
+
+
+        }
+    }
+}
