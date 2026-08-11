29e0270c731ee9926859df338be99c03d9a9a4d2
Aditya Upadhyay <goodboy281103@gmail.com>
2026-08-11 13:05:48 +0530

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_AccessModifiers
+{
+    internal class Student
+    {
+       
+            public string name = "Aditya";
+            private int Age = 22;
+            protected string Department = "MCA";
+            internal Double CGPA = 9.2;
+            protected internal string College = "GLA University";
+            private protected string Address = "Mathura";
+
+            public void Display()
+            {
+                Console.WriteLine("Student Details");
+                Console.WriteLine("__________________");
+                Console.WriteLine("NAME:" + name);
+                Console.WriteLine("AGE:" + Age);
+                Console.WriteLine("DEPARTMENT:" + Department);
+                Console.WriteLine("CGPA:" + CGPA);
+                Console.WriteLine("COLLEGE:" + College);
+                Console.WriteLine("ADDRESS:" + Address);
+
+            }
+            static void Main(string[] args)
+            {
+            Student s = new Student();
+            s.Display();
+
+            }
+
+
+
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+        
+
+    
+
+
+
+
+
+
