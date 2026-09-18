6a6e87bfb322ede64bc40d4d9b0f8e81cba028fd
Aditya Upadhyay <goodboy281103@gmail.com>
2026-09-18 09:13:03 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,36 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Mcaproject_1
+class program
 {
-    internal class Program 
+
+
+    delegate void MyDelegate(int a, int b);
+
+
+    static void CalculateSum(int a, int b)
+
     {
-        static void Main1(string[] args)
-        {
-            int rollno;
-            string name;
-            float marks;
+         
+        int sum = a + b;
 
-            Console.Write("Enter Student Name");
-             name = Console.ReadLine();
+        Console.WriteLine("Sum =" + sum);
 
-            Console.Write("Enter Student Roll  Number");
-             rollno = Convert.ToInt32(Console.ReadLine());
-            
-            Console.Write("Enter Student Marks");
-             marks = Convert.ToSingle(Console.ReadLine());
-
-            Console.WriteLine("\n---Student Details---");
-            Console.WriteLine("student Name:" + name);
-            Console.WriteLine("student Roll number:" + rollno);
-            Console.WriteLine("student Marks:" + marks);
-
-
-        }
-
-     
     }
+
+        static void Main()
+
+        { 
+
+        MyDelegate obj = new MyDelegate(CalculateSum);
+
+        obj(10, 20);
+
+    }
+
+
 }
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
